e186fbf80b66563c2bb9ec0df9d918f1cdea7506
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-08-30 10:38:39 +0300

    refactor: about page

--- src/prefs.ts
+++ src/prefs.ts
@@ -1,15 +1,36 @@
 import Adw from "gi://Adw";
+import Gdk from "gi://Gdk";
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import GObject from "gi://GObject";
 import Gtk from "gi://Gtk";
-
 import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
-
 import type { ExtensionMetadata } from "@girs/gnome-shell/extensions/extension";
+
 import { logger } from "./utils/logger.js";
 
-const LICENSE = "You can check out the LICENSE in the github page 🙂";
+const LICENSE = `MIT License
+
+Copyright (c) 2025 Dagim G. Astatkie
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
+
 type BlockedAppRowType = InstanceType<typeof BlockedAppRow>;
 
 const getTemplate = (name: string): string => {
@@ -523,10 +544,13 @@ const GeneralPage = GObject.registerClass(
 );
 
 interface AboutPageChildren {
+    _extensionIcon: Gtk.Image;
     _extensionName: Gtk.Label;
-    _extensionDescription: Gtk.Label;
-    _linkGithub: Gtk.LinkButton;
-    _linkIssues: Gtk.LinkButton;
+    _extensionVersion: Gtk.Label;
+    _linkWebsite: Gtk.Button;
+    _linkIssues: Gtk.Button;
+    _creditsRow: Adw.ExpanderRow;
+    _legalRow: Adw.ExpanderRow;
     _extensionLicense: Gtk.TextView;
 }
 
@@ -535,25 +559,59 @@ const AboutPage = GObject.registerClass(
         GTypeName: "TypescriptTemplateAboutPage",
         Template: getTemplate("AboutPage"),
         InternalChildren: [
+            "extensionIcon",
             "extensionName",
-            "extensionDescription",
-            "linkGithub",
+            "extensionVersion",
+            "linkWebsite",
             "linkIssues",
+            "creditsRow",
+            "legalRow",
             "extensionLicense",
         ],
     },
     class AboutPage extends Adw.PreferencesPage {
         setMetadata(metadata: ExtensionMetadata) {
             const children = this as unknown as AboutPageChildren;
+
+            // Set the icon from the project's assets
+            const iconPath = GLib.uri_resolve_relative(
+                import.meta.url,
+                "assets/icons/vicinae-symbolic.svg",
+                GLib.UriFlags.NONE,
+            );
+            if (iconPath) {
+                try {
+                    const iconFile = Gio.File.new_for_uri(iconPath);
+                    children._extensionIcon.set_from_file(iconFile.get_path());
+                } catch (_error) {
+                    // Fallback to a generic icon if the custom icon fails to load
+                    children._extensionIcon.set_from_icon_name(
+                        "application-x-executable",
+                    );
+                }
+            }
+
             children._extensionName.set_text(metadata.name);
-            children._extensionDescription.set_text(metadata.description);
+            children._extensionVersion.set_text(
+                `${metadata["version-name"] || metadata.version}`,
+            );
+
             if (metadata.url) {
-                children._linkGithub.set_uri(metadata.url);
-                children._linkIssues.set_uri(`${metadata.url}/issues`);
+                children._linkWebsite.connect("clicked", () => {
+                    Gtk.show_uri(null, metadata.url || "", Gdk.CURRENT_TIME);
+                });
+                children._linkIssues.connect("clicked", () => {
+                    Gtk.show_uri(
+                        null,
+                        `${metadata.url}/issues`,
+                        Gdk.CURRENT_TIME,
+                    );
+                });
             } else {
-                children._linkGithub.visible = false;
+                children._linkWebsite.visible = false;
                 children._linkIssues.visible = false;
             }
+
             children._extensionLicense.buffer.set_text(LICENSE, -1);
         }
     },
